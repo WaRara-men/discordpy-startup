6b69392bc3a0d19ac9c829955d252d5e7d573186
WaRara-men <71690839+WaRara-men@users.noreply.github.com>
2020-09-24 21:21:35 +0900

    implement neko

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,20 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-
+@bot.command()
+async def WaRa(ctx):
+    await ctx.send('即死')
+    
+@bot.command()
+async def ソルセルト(ctx):
+    await ctx.send('tintin')
+    
+@bot.command()
+async def しなめす(ctx):
+    await ctx.send('その香水のせいだよ～♪')
+    
+@bot.command()
+async def 筋肉マン(ctx):
+    await ctx.send('令和であります。')
+    
 bot.run(token)
