99768d5f78e5fe99ea46ec947e2f2fe42031347d
WaRara-men <71690839+WaRara-men@users.noreply.github.com>
2020-11-03 14:07:04 +0900

    ok

--- discordbot.py
+++ discordbot.py
@@ -31,9 +31,9 @@ async def 裏世界行く人(ctx):
 async def カミナさん(ctx):
     await ctx.send('いつもありがと～♪~♪ d(⌒o⌒)b♪~♪')
     
-    
 @bot.command()
 async def お疲れ様でした(ctx):
     await ctx.send('また参加してね(*￣▽￣)ﾉ~~')
 
+    
 bot.run(token)
